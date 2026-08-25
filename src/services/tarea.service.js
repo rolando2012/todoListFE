effc03f8cff7655d5a39dd7c5e6b59d36d9d8d49
rolando2012 <rolamdomr@gmail.com>
2026-08-25 15:06:54 -0400

    BOOT-5 Refactorizar getAll() para usar apiRequest

--- src/services/tarea.service.js
+++ src/services/tarea.service.js
@@ -1,21 +1,10 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+import { apiRequest } from "./apiClient.service";
 
 export async function getAll(){
-    const url = API_URL+'/tasks';
     try{
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        });
-        if(!response.ok){
-            throw new Error(`Respuesta de estado: ${response.status} `);
-        }
-        const result = await response.json();
-        return result;
+        return await apiRequest('/tasks', { method: 'GET' });
     }catch(error){
         console.error('Error en getAll: ', error.message);
+        throw error;
     }
 }
